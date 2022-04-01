0ef45be40e15181a9d5ff7029097825ba950c1e7
Nathan Krasney <natankrasney@gmail.com>
2022-04-01 22:33:02 +0300

    in code comments

--- first_tutorial.py
+++ first_tutorial.py
@@ -1,24 +1,40 @@
+# https://www.pygame.org/docs/tut/PygameIntro.html
 import sys, pygame
 
 pygame.init()
-size = width, height = 320, 240
-speed = [2, 2]
+size = width, height = 640, 480
+pos_ofset = [1, 1]
 black = 0, 0, 0
+
+# set_mode Initialize a window or screen for display. return Surface 
 screen = pygame.display.set_mode(size)
-ball = pygame.image.load("intro_ball.gif")
+#  ball is type Surface : https://www.pygame.org/docs/ref/surface.html
+ball = pygame.image.load("intro_ball.gif") 
+# ballreact is type Rect : Rect(left, top, width, height)
+# https://www.pygame.org/docs/ref/rect.html
 ballrect = ball.get_rect()
 
 while 1:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.QUIT: 
+            sys.exit()
 
-    ballrect = ballrect.move(speed)
+    # move return new Rect , https://www.pygame.org/docs/ref/rect.html#pygame.Rect.move
+    ballrect = ballrect.move(pos_ofset)
     if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
+        pos_ofset[0] = -pos_ofset[0]
 
     if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
+        pos_ofset[1] = -pos_ofset[1]
 
+    # fill Surface with a solid color , fill(color, rect=None, special_flags=0) -> Rect
+    # https://www.pygame.org/docs/ref/surface.html#pygame.Surface.fill
     screen.fill(black)
+
+    # draw one image onto another blit(source, dest, area=None, special_flags=0) -> Rect
+    # https://www.pygame.org/docs/ref/surface.html#pygame.Surface.blit
     screen.blit(ball, ballrect)
+
+    # https://www.pygame.org/docs/ref/display.html#pygame.display.flip
+    # Update the full display Surface to the screen . flip() -> None
     pygame.display.flip()
